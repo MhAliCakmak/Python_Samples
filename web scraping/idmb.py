66a8ee4a174fb75975b27b2d71510328bb9ab0eb
Mehmet ali Çakmak <79807829+mehmet5643@users.noreply.github.com>
2022-10-01 09:45:47 +0300

    idmb sended

---
+++ web scraping/idmb.py
@@ -0,0 +1,18 @@
+
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.imdb.com/chart/top'
+response = requests.get(url).content
+soup = BeautifulSoup(response, 'html.parser')
+
+liste=soup.find_all('tbody', {'class':'lister-list'})
+for i in liste:
+    basliklar=i.find_all('td', {'class':'titleColumn'})
+    for baslik in basliklar:
+        baslik=baslik.text
+        baslik=baslik.strip()
+        baslik=baslik.replace('\n','')
+        print(baslik)
+
+
