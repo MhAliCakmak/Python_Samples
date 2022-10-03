553bfd38d680ee781814e84c4d5223ba79b594be
Mehmet ali Çakmak <79807829+mehmet5643@users.noreply.github.com>
2022-10-03 09:31:53 +0300

    changed str-py

--- pandas/pandas-str.py
+++ pandas/pandas-str.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Read the CSV file into a DataFrame: df
-df = pd.read_csv('C:/Users/202803055/python/Python_Samples/pandas/all_seasons.csv')
+df = pd.read_csv('all_seasons.csv')
 
 
 result=df
