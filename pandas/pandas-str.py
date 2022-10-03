b17f84a0d18b83dea5666b7dc665c331dc80f97f
Mehmet ali Çakmak <79807829+mehmet5643@users.noreply.github.com>
2022-10-03 09:30:32 +0300

    pandas str-py 1 file sended

---
+++ pandas/pandas-str.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# Read the CSV file into a DataFrame: df
+df = pd.read_csv('C:/Users/202803055/python/Python_Samples/pandas/all_seasons.csv')
+
+
+result=df
+result=df.columns
+
+
+# Print data
+print(result)
